12a90c07141b39e0c4d563fb3c6186e8d56a7077
Andrey Gubaydullin <amattterasuuu@gmail.com>
2019-09-21 17:35:18 +0600

    decryption function for affine recursive cipher added

--- script.js
+++ script.js
@@ -71,7 +71,7 @@ function aCipher(src, dist) {
         } else errorDiv.style.display = 'none';
 
         let n = this.alphabet.length;
-        let alfaInvers = this.calcInverse(a, n);
+        let alfaInvers = calcInverse(a, n);
         let text = this.dst.value.toLowerCase();
         let decryptedText = '';
 
@@ -92,19 +92,9 @@ function aCipher(src, dist) {
         this.src.value = decryptedText;
     };
 
-    this.calcInverse = (a, n) => {
-
-        for (let i = 1; i < n; i++) {
-
-            if ((i * a) % n == 1)
-                return i;
-        }
-        return 1;
-    };
-
     this.defence = function (b, a) {
 
-        let errorDiv = document.getElementById('error');
+        let errorDiv = document.getElementById('error1');
 
         if (b != parseInt(b) || a != parseInt(a)) {
             errorDiv.style.display = 'block';
@@ -146,14 +136,14 @@ function aRCipher(src, dist) {
     this.encrypt = () => {
         let errorDiv = document.getElementById('error2');
 
-        let a1 = parseInt(this.a1Input.value)
-        let b1 = parseInt(this.b1Input.value)
-        let a2 = parseInt(this.a2Input.value)
-        let b2 = parseInt(this.b2Input.value)
-
-        // if (!this.defence(b, a)) {
-        //     return;
-        // } else errorDiv.style.display = 'none';
+        let a1 = parseInt(this.a1Input.value);
+        let b1 = parseInt(this.b1Input.value);
+        let a2 = parseInt(this.a2Input.value);
+        let b2 = parseInt(this.b2Input.value);
+;
+        if (!this.defence(b1, a1) || !this.defence(b2, a2)) {
+            return;
+        } else errorDiv.style.display = 'none';
 
         let n = this.alphabet.length;
         let text = this.src.value.toLowerCase();
@@ -164,17 +154,13 @@ function aRCipher(src, dist) {
         for (let i = 0; i < text.length; i++) {
 
             if (count === 0) {
-                a = 5;
-                b = 2;
-                a1 = 5;
-                b1 = 2;
+                a = a1;
+                b = b1;
             }
 
             if (count === 1) {
-                a = 7;
-                b = 4;
-                a2 = 7;
-                b2 = 4;
+                a = a2;
+                b = b2;
             }
 
             if (count > 1) {
@@ -183,7 +169,7 @@ function aRCipher(src, dist) {
                 b1 = b2;
                 a1 = a2;
                 b2 = b;
-                a2 = a
+                a2 = a;
             }
 
             let c = text.charAt(i);
@@ -203,7 +189,106 @@ function aRCipher(src, dist) {
 
         this.dst.value = encryptedText;
     };
+
+    this.decrypt = () => {
+        let errorDiv = document.getElementById('error2');
+
+        let a1 = parseInt(this.a1Input.value);
+        let b1 = parseInt(this.b1Input.value);
+        let a2 = parseInt(this.a2Input.value);
+        let b2 = parseInt(this.b2Input.value);
+
+        if (!this.defence(b1, a1) || !this.defence(b2, a2)) {
+            return;
+        } else errorDiv.style.display = 'none';
+
+        let n = this.alphabet.length; //26
+        let text = this.dst.value.toLowerCase();
+        let decryptedText = '';
+        let count = 0;
+
+        for (let i = 0; i < text.length; i++) {
+
+            if (count === 0) {
+                a = a1;
+                b = b1; 
+            }
+
+            if (count === 1) {
+                a = a2;
+                b = b2;
+            }
+
+            if (count > 1) {
+                a = (a1 * a2) % n;
+                b = (b1 + b2) % n;
+                b1 = b2;
+                a1 = a2;
+                b2 = b;
+                a2 = a;
+            }
+
+            let alfaInvers = calcInverse(a, n);
+            let c = text.charAt(i);
+            let position = this.alphabet.indexOf(c);
+
+
+            if (position < 0) {
+                decryptedText += c;
+                continue;
+            }
+
+            let newPosition = (alfaInvers * (parseInt(position) - b)) % n;
+
+            if (newPosition < 0) {
+                newPosition += 26;
+            }
+
+            let newSymbol = this.alphabet.charAt(newPosition);
+
+            decryptedText += newSymbol;
+            count++;
+        }
+
+        this.src.value = decryptedText;
+    };
+
+    this.defence = function (b, a) {
+
+        let errorDiv = document.getElementById('error2');
+
+        if (b != parseInt(b) || a != parseInt(a)) {
+            errorDiv.style.display = 'block';
+            errorDiv.innerHTML = 'A и K должны быть целыми числами!';
+            return false;
+        }
+
+        if (a <= 0 || b < 0) {
+            errorDiv.style.display = 'block';
+            errorDiv.innerHTML = 'A и B не могут быть отрицательными! <br> A не может быть равен нулю!';
+            return false;
+        }
+
+        if (isCoprime(a, b) == 1)
+            return true;
+        else {
+            errorDiv.style.display = 'block';
+            errorDiv.innerHTML = 'Числа не взаимно простые!';
+            return false;
+        }
+    }
 }
+
+function calcInverse (a, n) {
+
+    for (let i = 1; i < n; i++) {
+
+        if ((i * a) % n == 1)
+            return i;
+    }
+    return 1;
+};
+
 
 function isCoprime(a, b) {
     return b ? isCoprime(b, a % b) : a;
@@ -211,5 +296,10 @@ function isCoprime(a, b) {
 
 setInterval(() => {
     let errorDiv = document.getElementById('error1');
+    errorDiv.style.display = 'none';
+}, 5000);
+
+setInterval(() => {
+    let errorDiv = document.getElementById('error2');
     errorDiv.style.display = 'none';
 }, 5000);
